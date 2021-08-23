d4226f5db53fa7c068faa3f0a621a962ec70578e
EnriqueCaBe <33118290+EnriqueCaBe@users.noreply.github.com>
2021-08-23 18:02:24 +0200

    commit

--- src/main/resources/static/js/delete.js
+++ src/main/resources/static/js/delete.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     getTabacos();
-    getFormatos();
     getMarcas();
   });
   
@@ -30,98 +29,6 @@ $(document).ready(function () {
     });
   }
   
-  async function getFormatos() {
-    const formatos = await getJsonFormatos();
-  
-    formatos.map((formato) => {
-      document
-        .getElementById("formato")
-        .insertAdjacentHTML(
-          "beforeend",
-          `<option value="${formato.id}">${formato.gramos}g | ${
-            formato.precio
-          } ${"&#x20ac"}</option>`
-        );
-      document
-        .getElementById("formato1")
-        .insertAdjacentHTML(
-          "beforeend",
-          `<option value="${formato.id}">${formato.gramos}g | ${
-            formato.precio
-          } ${"&#x20ac"}</option>`
-        );
-    });
-  }
-  function getJsonFormatos() {
-    return new Promise((resolve) => {
-      $.ajax({
-        url: `/formato/all`,
-        type: "GET",
-        dataType: "json",
-        success: function (data) {
-          resolve(data);
-        },
-        error: function () {},
-      });
-    });
-  }
-  
-  async function submit() {
-    var assFormato = `{"tabaco_id":${
-      document.getElementById("tabaco").value
-    }, "formato_id":${document.getElementById("formato").value} }`;
-    await insertAssFormato(assFormato);
-    limpiarCampos();
-  }
-  
-  async function submit1() {
-    await mergeMarcaFormato(
-      document.getElementById("marca").value,
-      document.getElementById("formato1").value
-    );
-  }
-  
-  function mergeMarcaFormato(marca, formato) {
-    return new Promise((resolve) => {
-      fetch(`/ass_formato/all/${marca}/${formato}`, {
-        method: "POST",
-      })
-        .then(function (response) {
-          if (response.ok) {
-            toastr.success("Merge correcto");
-          } else {
-            toastr.error("Merge erroneo");
-          }
-        })
-        .then((res) => resolve(res));
-    });
-  }
-  
-  function limpiarCampos() {
-    document.getElementById("formato").value = 0;
-    document.getElementById("tabaco").value = 0;
-  }
-  
-  function insertAssFormato(assFormato) {
-    return new Promise((resolve) => {
-      fetch(`/ass_formato`, {
-        method: "POST",
-        body: assFormato,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then(function (response) {
-          if (response.ok) {
-            toastr.success("Añadido correctamente");
-          } else {
-            toastr.error("Ya existe");
-          }
-        })
-        .then((res) => resolve(res));
-    });
-  }
-  
   async function getMarcas(){
     const marcas = await getJsonMarcas();
     var letras_añadidas= [];
@@ -147,4 +54,12 @@ $(document).ready(function () {
         },
       });
     });
+  }
+
+  async function submit1() {
+    var assFormato = `{"tabaco_id":${
+      document.getElementById("tabaco").value
+    }, "formato_id":${document.getElementById("formato").value} }`;
+    await insertAssFormato(assFormato);
+    limpiarCampos();
   }
